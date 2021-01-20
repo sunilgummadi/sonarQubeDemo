e7b4ed710f20cddb8cf1c433a9b8734bc4c19b20
sunil kumar <sunil1998kg@gmail.com>
2021-01-20 12:15:50 +0530

    changes

--- src/main/java/com/springbootsonarqube/controller/DemoController.java
+++ src/main/java/com/springbootsonarqube/controller/DemoController.java
@@ -1,8 +1,11 @@
 package com.springbootsonarqube.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,15 @@ public class DemoController {
 	@PutMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody User user){
 		return new ResponseEntity<>(loginService.save(user),HttpStatus.OK);
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<User>> getALlUsers(){
+		try {
+			List<User> users = loginService.getAll();
+			if(users.size() == 0)
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 }
